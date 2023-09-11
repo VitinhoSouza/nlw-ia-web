628ad865bbc21fcb2defc3d5e5a09f32a22e7b37
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2023-09-11 20:56:40 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export function App() {
+
+  return (
+    <h1>Hello World</h1>
+  )
+}
